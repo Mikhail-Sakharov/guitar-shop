2dfe8d6352ab6fb08b5c5c3c113bdc0601b3be57
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-02-15 17:50:36 +0300

    Исправил заголовки авторизации

--- guitar-shop/src/services/api.ts
+++ guitar-shop/src/services/api.ts
@@ -17,7 +17,7 @@ const REQUEST_TIMEOUT = 5000;
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
-    timeout: REQUEST_TIMEOUT,
+    timeout: REQUEST_TIMEOUT
   });
 
   api.interceptors.request.use(
@@ -25,7 +25,7 @@ export const createAPI = (): AxiosInstance => {
       const token = getToken();
 
       if (token && config.headers) {
-        config.headers['x-token'] = token;
+        config.headers['Authorization'] = `Bearer ${token}`;
       }
 
       return config;
